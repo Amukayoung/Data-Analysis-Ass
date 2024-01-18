c4f4b1376db4baeab0fd3cfca50a75b0aaf3518b
Andrew Amuka | Software Dev | JavaScript & Java <105834291+Amukayoung@users.noreply.github.com>
2024-01-18 03:45:34 +0300

    Added more api endpoints

--- danalysis/urls.py
+++ danalysis/urls.py
@@ -15,6 +15,16 @@ urlpatterns = [
         "inlinequestionresponses/lastyear",
         views.monthly_inline_question_feedback_last_year,
     ),
+    path("inlinequestionfeedbackcount/", views.inline_question_feedback_count),
+    path("workercount/", views.active_worker_count),
+    path("chapterquestionfeedbackcount/", views.chapter_question_feedback_count),
+    path("uniqueagecounts/", views.unique_age_counts),
+    path("monthfeedbackcount", views.monthly_question_feedback_counts_2023),
+    path("topansweredinlinequestions", views.top_answered_questions),
+    path("routedevices/", views.route_devices),
+    path("workercreatedat", views.worker_device_created_at),
+    path("workerroutecount/", views.worker_route_count),
+    path("organisationworkercount/", views.organisation_worker_count),
     path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
 ]
 
--- danalysis/views.py
+++ danalysis/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Worker, Sector, Organisation, Route, Device
-from questions.models import InlineQuestionFeedback
+from questions.models import InlineQuestionFeedback, QuestionFeedback, InlineQuestion
 from .serializers import (
     WorkerSerializers,
     SectorSerializers,
@@ -93,20 +93,16 @@ def active_users_last_year_grouped_by_month(request, format=None):
 @api_view(["GET"])
 def top_routes_last_year(request, format=None):
     try:
-        # Get the current year and month
         current_year = datetime.now().year
         current_month = datetime.now().month
 
-        # Calculate the start and end dates for the last year
         start_date = datetime(current_year - 1, current_month, 1)
         end_date = datetime(current_year, current_month, 1)
 
-        # Query to get workers instances that occurred last year
         workers_last_year = Worker.objects.filter(
             createdAt__gte=start_date, createdAt__lt=end_date
         )
 
-        # Group by routeId and count occurrences, including route name
         top_routes_last_year = (
             workers_last_year.values("routeId__id", "routeId__route")
             .annotate(total_count=Count("id"))
@@ -123,17 +119,6 @@ def top_routes_last_year(request, format=None):
 
     except Exception as e:
         return Response({"Error": f"An error occurred: {str(e)}"})
-
-
-from django.db.models import Count
-from django.db.models.functions import ExtractMonth
-
-
-from django.db.models import Count
-from django.db.models.functions import ExtractMonth
-from django.db.models import Value, IntegerField
-from django.db.models import Q
-from django.db.models import Case, When
 
 
 @api_view(["GET"])
@@ -168,3 +153,197 @@ def monthly_inline_question_feedback_last_year(request, format=None):
         )
     except Exception as e:
         return Response({"Error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(["GET"])
+def unique_age_counts(request, format=None):
+    try:
+        age_counts = (
+            Worker.objects.values("age")
+            .annotate(worker_count=Count("id"))
+            .order_by("age")
+        )
+
+        result = [
+            {"age": entry["age"], "worker_count": entry["worker_count"]}
+            for entry in age_counts
+        ]
+
+        return Response(
+            {
+                "Success": True,
+                "Status": 200,
+                "data": {
+                    "UniqueAgeCounts": result,
+                },
+            }
+        )
+    except Exception as e:
+        return Response({"Error": str(e)}, status=500)
+
+
+def inline_question_feedback_count(request):
+    feedback_count = InlineQuestionFeedback.objects.aggregate(total_count=Count("id"))[
+        "total_count"
+    ]
+    return JsonResponse({"total_count": feedback_count})
+
+
+def active_worker_count(request):
+    worker_count = Worker.objects.aggregate(total_count=Count("id"))["total_count"]
+    return JsonResponse({"total_count": worker_count})
+
+
+def chapter_question_feedback_count(request):
+    chapter_feedback_count = QuestionFeedback.objects.aggregate(
+        total_count=Count("id")
+    )["total_count"]
+    return JsonResponse({"total_count": chapter_feedback_count})
+
+
+@api_view(["GET"])
+def unique_age_counts(request, format=None):
+    try:
+        age_counts = (
+            Worker.objects.values("age")
+            .annotate(worker_count=Count("id"))
+            .order_by("age")
+        )
+
+        result = [
+            {"age": entry["age"], "worker_count": entry["worker_count"]}
+            for entry in age_counts
+        ]
+
+        return JsonResponse(
+            {
+                "Success": True,
+                "Status": 200,
+                "data": {
+                    "UniqueAgeCounts": result,
+                },
+            }
+        )
+    except Exception as e:
+        return JsonResponse({"Error": str(e)}, status=500)
+
+
+@api_view(["GET"])
+def monthly_question_feedback_counts_2023(request):
+    try:
+        start_date = datetime(2023, 1, 1)
+        end_date = datetime(2023, 12, 31, 23, 59, 59)
+
+        feedback_counts_2023 = (
+            QuestionFeedback.objects.filter(
+                createdAt__gte=start_date, createdAt__lte=end_date
+            )
+            .annotate(month=ExtractMonth("createdAt"))
+            .annotate(year=ExtractYear("createdAt"))
+            .values("year", "month")
+            .annotate(total_feedback=Count("id"))
+            .order_by("year", "month")
+        )
+
+        return Response(
+            {
+                "Success": True,
+                "Status": 200,
+                "data": {"MonthlyQuestionFeedbackCounts2023": feedback_counts_2023},
+            }
+        )
+    except Exception as e:
+        return Response({"Error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(["GET"])
+def top_answered_questions(request):
+    try:
+        top_questions = (
+            InlineQuestion.objects.annotate(
+                feedback_count=Count("inline_question_feedback")
+            )
+            .order_by("-feedback_count")[:10]
+            .values("name", "feedback_count")
+        )
+
+        return Response(
+            {
+                "Success": True,
+                "Status": 200,
+                "data": {"TopAnsweredQuestions": list(top_questions)},
+            }
+        )
+    except Exception as e:
+        return Response({"Error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+def route_devices(request):
+    routes_data = Worker.objects.values("routeId__route", "deviceId__id").order_by(
+        "routeId__route"
+    )
+
+    routes_dict = {}
+    for route_data in routes_data:
+        route = route_data["routeId__route"]
+        device_id = route_data["deviceId__id"]
+
+        if route not in routes_dict:
+            routes_dict[route] = []
+
+        routes_dict[route].append(device_id)
+
+    return JsonResponse({"route_devices": routes_dict})
+
+
+@api_view(["GET"])
+def worker_device_created_at(request):
+    workers = Worker.objects.values("deviceId__id", "createdAt")
+    return Response({"data": workers})
+
+
+def worker_route_count(request):
+    try:
+        route_counts = Route.objects.annotate(
+            worker_count=Count("workers_route")
+        ).order_by("-worker_count")[:12]
+
+        data = {
+            "Success": True,
+            "Status": 200,
+            "data": {
+                "WorkerRouteCounts": [
+                    {"route": route.route, "worker_count": route.worker_count}
+                    for route in route_counts
+                ]
+            },
+        }
+        return JsonResponse(data)
+    except Exception as e:
+        error_data = {"Success": False, "Status": 500, "Error": str(e)}
+        return JsonResponse(error_data, status=500)
+
+
+def organisation_worker_count(request):
+    try:
+        organisations = Organisation.objects.annotate(
+            worker_count=Count("workers_organisation")
+        ).order_by("name")
+
+        data = {
+            "Success": True,
+            "Status": 200,
+            "data": {
+                "OrganisationWorkerCounts": [
+                    {
+                        "organisation": organisation.name,
+                        "worker_count": organisation.worker_count,
+                    }
+                    for organisation in organisations
+                ]
+            },
+        }
+        return JsonResponse(data)
+    except Exception as e:
+        error_data = {"Success": False, "Status": 500, "Error": str(e)}
+        return JsonResponse(error_data, status=500)
